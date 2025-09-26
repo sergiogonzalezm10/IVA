4b8a9b8089dac63e722ca16d5ff9effcfdb01056
sergio.gonzalezm <sergio.gonzalezm@106-09.info2>
2025-09-26 12:32:13 +0200

    IVA

--- src/iva/Iva.java
+++ src/iva/Iva.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 public class Iva {
 
 	public static void main(String[] args) {
-		final double IVA = 0.21;
-		double precio;
-		double preciofinal;
+		final double IVA = 0.21;		//Constante que almacena el valor del IVA
+		double precio;					//Variable que almacena el precio
+		double preciofinal;				//Variable qu almacena el preciofinal
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Precio: ");
 		precio = sc.nextDouble();
 		
-		preciofinal = ( 1 + IVA) * precio;
+		preciofinal = ( 1 + IVA) * precio;	//Operación para calcular el precio final
 		System.out.println("Precio final con IVA: " + preciofinal + " €");
 		sc.close();
 	}
